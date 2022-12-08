12f1a8b918faef7bddc7326d66385d796a8314e4
Hubert Yu <hubert12161999@gmail.com>
2022-12-08 02:14:49 +0000

    Create a grid function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+const container = document.querySelector('.grid-container');
+
+function createGrid(size) {
+  container.style.display = 'grid';
+  container.style.height = '600px';
+  container.style.width = '600px';
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size ** 2; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
+  }
+}
