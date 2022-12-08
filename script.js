c53b8ee710f67223154096f0c19ee5fc130d8737
Hubert Yu <hubert12161999@gmail.com>
2022-12-08 04:47:22 +0000

    Add drawing functionality and different modes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('.grid-container');
+const rainbowButton = document.querySelector('.rainbow-btn');
+const gridButton = document.querySelector('.grid-btn');
+const eraserButton = document.querySelector('.eraser-btn');
+let brushColour = 'black';
 
 function createGrid(size) {
   container.style.display = 'grid';
@@ -11,5 +15,44 @@ function createGrid(size) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     container.appendChild(div);
+    div.addEventListener('mouseover', setColour);
   }
+}
+
+createGrid(24);
+
+function setColour(e) {
+  switch (brushColour) {
+    case 'black':
+      e.target.style.backgroundColor = 'black';
+      break;
+    case 'rainbow':
+      e.target.style.backgroundColor = randomRGB();
+      break;
+    case 'erase':
+      e.target.style.backgroundColor = 'rgb(255, 250, 240)';
+      break;
+    default:
+      break;
+  }
+}
+
+gridButton.addEventListener('click', () => {
+  container.classList.toggle('grid-gap');
+});
+
+eraserButton.addEventListener('click', () => {
+  brushColour = 'erase';
+});
+
+rainbowButton.addEventListener('click', () => {
+  brushColour = 'rainbow';
+});
+
+function random256() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomRGB() {
+  return `rgb(${random256()}, ${random256()}, ${random256()})`;
 }
